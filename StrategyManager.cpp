28c2bc9a368e465aca8c0964ed0644b8e824fab1
Hagar Paytan <hagarleap@mail.tau.ac.il>
2025-04-26 12:00:42 +0300

    algo updates + makefile and readme

--- StrategyManager.cpp
+++ StrategyManager.cpp
@@ -10,10 +10,15 @@ TankAction StrategyManager::getAction(int tankId, const Tank& tank, const Board&
         std::cout << tank.toString() << std::endl; //TODO: delete
         TankAction rawAction = strategies[tankId]->decideAction(tank, board, shells);
         std::cout << to_string(rawAction) << std::endl; //TODO: delete
-        std::string debug;
-        TankAction adjusted = applyCommonSense(tank, board, shells, rawAction, debug);
-        std::cout << to_string(adjusted) << std::endl; //TODO: delete
-        return adjusted;
+
+        if (useCommonSense) {
+            std::string debug;
+            TankAction adjusted = applyCommonSense(tank, board, shells, rawAction, debug);
+            std::cout << to_string(adjusted) << std::endl; //TODO: delete
+            return adjusted;
+        } else {
+            return rawAction;
+        }
     }
     return TankAction::None;
 }
